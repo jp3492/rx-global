b8cefb6d1bfbea7fa0a734e02b1a564e74f55f91
Jan-Philipp <jpm3492@gmail.com>
2020-06-30 16:51:32 +0200

    init

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,8 @@
 export declare const setGlobalState: (id: string, value: any) => void;
 export declare const getGlobalState: (id: string) => any;
+export declare const updateGlobalState: (id: string, cb: Function) => void;
+export declare const subGlobalState: (id: string, cb: Function) => void;
+export declare const unsubGlobalState: (id: string, cb: Function) => void;
+export declare const resetGlobalState: (id: string) => void;
+export declare const resetGlobalStates: (ids: string[]) => void;
 export declare const useGlobalState: (id: string, initialValue?: any) => [any, Function];
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,11 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("react");
 var stores = {};
@@ -7,23 +14,31 @@ var Store = /** @class */ (function () {
         var _this = this;
         this.value = "";
         this.setters = [];
+        this.setInitialValue = function (value) {
+            _this.initialValue = value;
+            _this.setValue(value);
+        };
         this.setValue = function (value) {
             _this.value = value;
             _this.setters.forEach(function (setter) { return setter(_this.value); });
         };
         this.addSetter = function (setter) {
-            _this.setters = _this.setters.concat([setter]);
+            _this.setters = __spreadArrays(_this.setters, [setter]);
         };
         this.unsubscribe = function (setter) {
             _this.setters = _this.setters.filter(function (s) { return s !== setter; });
+            return _this.setters.length;
         };
     }
     return Store;
 }());
+var initializeState = function (id, value) {
+    stores[id] = new Store();
+    stores[id].setInitialValue(value);
+};
 exports.setGlobalState = function (id, value) {
     if (!stores[id]) {
-        stores[id] = new Store();
-        stores[id].setValue(value);
+        initializeState(id, value);
     }
     else {
         stores[id].setValue(value);
@@ -37,17 +52,47 @@ exports.getGlobalState = function (id) {
         console.error("No gloabl value for id: " + id);
     }
 };
+exports.updateGlobalState = function (id, cb) {
+    var store = stores[id];
+    if (!store) {
+        console.error("No store initialized for " + id);
+    }
+    else {
+        var updatedValue = cb(store.value);
+        store.setValue(updatedValue);
+    }
+};
+exports.subGlobalState = function (id, cb) {
+    stores[id].subscribe(cb);
+};
+exports.unsubGlobalState = function (id, cb) {
+    stores[id].unsubscribe(cb);
+};
+exports.resetGlobalState = function (id) {
+    var store = stores[id];
+    store.setValue(store.initialValue);
+};
+exports.resetGlobalStates = function (ids) {
+    ids.forEach(function (id) {
+        var store = stores[id];
+        store.setValue(store.initialValue);
+    });
+};
 exports.useGlobalState = function (id, initialValue) {
     if (initialValue === void 0) { initialValue = ""; }
     var _a = react_1.useState(""), _ = _a[0], set = _a[1];
     if (!stores.hasOwnProperty(id)) {
-        stores[id] = new Store();
-        stores[id].setValue(initialValue);
+        initializeState(id, initialValue);
     }
     if (!stores[id].setters.includes(set)) {
         stores[id].addSetter(set);
     }
     var _b = stores[id], value = _b.value, setValue = _b.setValue, unsubscribe = _b.unsubscribe;
-    react_1.useEffect(function () { return function () { return unsubscribe(set); }; }, []);
+    react_1.useEffect(function () { return function () {
+        var setters = unsubscribe(set);
+        if (setters.length === 0) {
+            delete stores[id];
+        }
+    }; }, []);
     return [value, setValue];
 };
